df119c0e603a08f508c6a3bd1f29bb1c788cbfdc
belofte2025 <belofte2025@outlook.com>
2026-01-24 19:05:59 +0000

    updatedfisesinventory

--- client/app/suppliers/[id]/stock-adjustments/page.tsx
+++ client/app/suppliers/[id]/stock-adjustments/page.tsx
@@ -1,15 +1,5 @@
-import DashboardLayout from "@/components/layout/DashboardLayout";
 import StockAdjustmentPage from "@/components/pages/suppliers/StockAdjustmentPage";
 
-interface StockAdjustmentRouteProps {
-  params: Promise<{ id: string }>;
-}
-
-export default async function StockAdjustmentRoute({ params }: StockAdjustmentRouteProps) {
-  const { id } = await params;
-  return (
-    <DashboardLayout>
-      <StockAdjustmentPage supplierId={id} />
-    </DashboardLayout>
-  );
+export default function Page({ params }: { params: { id: string } }) {
+  return <StockAdjustmentPage supplierId={params.id} />;
 }
